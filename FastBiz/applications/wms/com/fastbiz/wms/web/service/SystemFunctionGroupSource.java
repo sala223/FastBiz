ecf45c539fdc08b0491bc3ccf4e21893b4574538
sala223 <sala223@msn.com>
2012-08-28 13:52:47 +0800

    fix compilation error

--- FastBiz/applications/wms/com/fastbiz/wms/web/service/SystemFunctionGroupSource.java
+++ FastBiz/applications/wms/com/fastbiz/wms/web/service/SystemFunctionGroupSource.java
@@ -1,4 +1,18 @@
 package com.fastbiz.wms.web.service;
 
+import com.fastbiz.wms.web.model.ui.FunctionGroup;
 
-public class SystemFunctionGroupSource implements FunctionGroupSource{}
+
+public class SystemFunctionGroupSource implements FunctionGroupSource{
+
+    @Override
+    public FunctionGroup loadFunctionGroup(){
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getParentFunctionGroupId(){
+        // TODO Auto-generated method stub
+        return null;
+    }}
